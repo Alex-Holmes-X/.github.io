3073767889d4bc0e94f8d41c1688d433d7016c33
Alex Holmes <alex_j_holmes@outlook.com>
2023-09-02 22:05:37 +0100

    Site ready - JS to update

---
+++ javaScript.js
@@ -0,0 +1,9 @@
+let focus = document.getElementById("about-me-description");
+
+
+
+// This handler will be executed every time the cursor
+// is moved over a different list item
+// test.addEventListener("mouseover",(event) => {
+      // highlight the mouseover target
+    //   event.target.style.color = "orange";
