75ea1f68bd51f2d2c53d082adf6e35db89aca987
Alex Holmes <alex_j_holmes@outlook.com>
2023-09-04 12:35:47 +0100

    Final Draft Live

--- javaScript.js
+++ javaScript.js
@@ -1,9 +1,37 @@
-let focus = document.getElementById("about-me-description");
+ /* function mouseOn() {
+    var x = document.getElementById("aboutMe");
+    x.style.backgroundColor = "white"; 
+}
+
+function mouseOff() {
+  var x = document.getElementById("aboutMe");
+    x.style.backgroundColor = "rgba(0, 216, 194, 0.577)";
+
+} */
 
 
 
-// This handler will be executed every time the cursor
-// is moved over a different list item
-// test.addEventListener("mouseover",(event) => {
-      // highlight the mouseover target
-    //   event.target.style.color = "orange";
+/* This section highlights the section HTML and increases the size of the text */
+function highlightOn () {
+    let focus = document.getElementById("projects");
+    // focus.style.color = 'rgb(115, 115, 115)';
+    focus.style.fontSize = '25px';
+}
+
+function highlightOff () {
+    let focus = document.getElementById("projects");
+    // focus.style.color = '';
+    focus.style.fontSize = ''
+}
+
+function highlightOnSkills () {
+    let focus = document.getElementById("skills");
+    // focus.style.color = 'rgb(115, 115, 115)';
+    focus.style.fontSize = '25px';
+}
+
+function highlightOffSkills () {
+    let focus = document.getElementById("skills");
+    // focus.style.color = '';
+    focus.style.fontSize = ''
+}
